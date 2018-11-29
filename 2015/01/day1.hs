68b1b1db116f55fa192f2cfbead364e88abfe62d
Killian Coddington <kcoddington73@gmail.com>
2018-11-29 00:25:48 -0500

    Refactor 2015 day 01

--- 2015/01/day1.hs
+++
@@ -1,8 +0,0 @@
-perenthesisToChange :: Char -> Int
-perenthesisToChange p = if p == ')'
-    then -1
-    else 1
-
-zeroPosition :: Int -> [Int] -> Int -> Int
-zeroPosition 0 _ p = p
-zeroPosition f (x:xs) p = zeroPosition (f + x) xs (p + 1)  
